0face480854a97ad5dbfb860105b8588cf3d96d2
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-05-21 19:07:30 +0200

    Time: 122 ms (32.20%), Space: 37 MB (41.16%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cs
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cs
@@ -1,16 +1,8 @@
 public class Solution {
     public double Average(int[] salary) {
-     double avg = 1;
-        double sum = 0;
-        int max = salary.Max();
-        int min = salary.Min();
-        double n = salary.Length;
-        for (int i = 0; i < n; i++)
-        {
-            if (salary[i] == max || salary[i] == min) continue;
-            sum += salary[i];
-        }
-        avg = sum / (n - 2);
-        return avg;
+     return salary.OrderBy(s => s)
+                     .Skip(1)
+                     .Take(salary.Length-2)
+                     .Average();
     }
 }
