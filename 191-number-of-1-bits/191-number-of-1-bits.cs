dd7936908ae99c46b386a5cbdd2682b60f51bd93
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-05-23 14:56:23 +0200

    Time: 45 ms (7.67%), Space: 22.1 MB (97.51%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cs
+++ 191-number-of-1-bits/191-number-of-1-bits.cs
@@ -3,7 +3,6 @@ public class Solution
     public  int HammingWeight(uint n)
     {
         int count = 0;
-        string number = n.ToString();
         while (n > 0)
         {
             if ((n & 1) >= 1) count++;
