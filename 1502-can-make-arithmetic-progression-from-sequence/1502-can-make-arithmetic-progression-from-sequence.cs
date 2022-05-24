a351a7edae571777b36844d37def82d5acb64b9b
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-05-24 12:11:33 +0200

    Time: 169 ms (15.06%), Space: 39.4 MB (76.57%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cs
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cs
@@ -2,8 +2,8 @@ public class Solution {
     public bool CanMakeArithmeticProgression(int[] arr) {
         Array.Sort(arr);
         int n=arr.Length;
+        int difference=arr[1]-arr[0];
         for(int i=0;i<n-1;++i){
-            int difference=arr[1]-arr[0];
             if(arr[i+1]-arr[i]!=difference)return false;
         }
         return true;
