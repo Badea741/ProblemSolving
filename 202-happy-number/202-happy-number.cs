1157f6c1c10cbb7b7af37353601cc1f6091f4ae0
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-05-24 13:18:55 +0200

    Time: 76 ms (5.36%), Space: 27.3 MB (84.51%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsHappy(int n) {
+        int count = 0;
+        while (n > 1)
+        {
+            int sum = 0;
+            int n2 = n;
+            while (n2 >= 1)
+            {
+                sum += (int)Math.Pow(n2 % 10, 2);
+                n2 /= 10;
+            }
+            n = sum;
+            count++;
+            if (count >= 1000) return false;
+        }
+        return true;
+    }
+}
