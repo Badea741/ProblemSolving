f0d5f76f8724e5a6d6f755c80e33c88306a7fc9c
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-05-27 15:06:23 +0200

    Time: 212 ms (30.74%), Space: 42.1 MB (69.65%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] NextGreaterElement(int[] nums1, int[] nums2) {List<int> result = new();
+       Stack<int> stack = new();
+        Dictionary<int, int> dict = new();
+        foreach (int num in nums2)
+        {
+            while (stack.Count != 0 && num > stack.Peek())
+            {
+                dict[stack.Pop()] = num;
+            }
+            stack.Push(num);
+        }
+        int i = 0;
+        foreach (int num in nums1)
+        {
+            nums1[i] = dict.GetValueOrDefault(num, -1);
+            i++;
+        }
+        return nums1;
+    }
+}
