2e8ae716db353b5832576a7d78a41904adfb11df
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-05-27 12:05:33 +0200

    Time: 188 ms (74.86%), Space: 45.6 MB (45.81%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cs
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cs
@@ -18,12 +18,20 @@ public class Node {
 */
 
 public class Solution {
-    List<int> result=new();
     public IList<int> Preorder(Node root) {
-        if(root==null)return result;
-        result.Add(root.val);
-        foreach(Node node in root.children)
-            Preorder(node);
+        if (root == null) return new List<int>();
+        List<int> result = new();
+        Stack<Node> stack = new();
+        stack.Push(root);
+        while (stack.Count != 0)
+        {
+            root=stack.Pop();
+            result.Add(root.val);
+            foreach (Node node in root.children.Reverse())
+            {
+                stack.Push(node);
+            }
+        }
         return result;
     }
 }
