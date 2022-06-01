79d9fb879387c2c2cb67f5f33a6e3d9e34a558ac
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-06-02 00:22:11 +0200

    Time: 90 ms (90.44%), Space: 39.2 MB (20.93%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public char FindTheDifference(string s, string t) {
+        byte[] arr1=new byte[256];
+        byte[] arr2=new byte[256];
+        foreach(char c in s){
+            arr1[c]++;
+        }
+        foreach(char c in t){
+            arr2[c]++;
+        }
+        foreach(char c in t){
+            if(arr1[c]<arr2[c])return c;
+        }
+        return 'a';
+    }
+}
