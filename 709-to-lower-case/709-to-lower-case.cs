14af8d274f1f7dcd07795c0aafff84d4546e1287
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-06-02 15:42:54 +0200

    Time: 137 ms (11.64%), Space: 35.6 MB (94.18%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cs
+++ 709-to-lower-case/709-to-lower-case.cs
@@ -1,5 +1,20 @@
-public class Solution {
-    public string ToLowerCase(string s) {
-        return s.ToLower();
+using System.Text;
+
+public class Solution
+{
+    public  string ToLowerCase(string s)
+    {
+         int n = s.Length;
+        StringBuilder ans = new();
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] < 91&&s[i]>=65)
+            {
+                ans.Append((char)(s[i] + 32));
+                continue;
+            };
+            ans.Append(s[i]);
+        }
+        return ans.ToString();
     }
 }
