4edc1f26857293b0515f1822bd4c72ef0f26dfa8
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-06-03 20:10:33 +0200

    Time: 92 ms (70.64%), Space: 39.6 MB (35.82%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cs
+++ 242-valid-anagram/242-valid-anagram.cs
@@ -3,21 +3,19 @@ public class Solution {
         if(t.Length!=s.Length)return false;
         Dictionary<int,int> dict=new();
         Dictionary<int,int> dict1=new();
-       foreach(char c in s){
-            if(dict.ContainsKey(c)){dict[c]++;continue;}
-            dict[c]=0;
-            dict[c]++;
-       }
-        foreach(char c in t){
-            if(!dict.ContainsKey(c))return false;
-            if(dict1.ContainsKey(c)){dict1[c]++;continue;}
-            dict1[c]=0;
-            dict1[c]++;
-        }
-        foreach(char c in t){
-            if(dict[c]!=dict1[c])return false;
+      for(int i =0;i<s.Length;i++){
+          if(dict.ContainsKey(s[i])){
+              dict[s[i]]++;
+          }
+          else dict[s[i]]=1;
+          if(dict.ContainsKey(t[i])){
+              dict[t[i]]--;
+          }
+          else dict[t[i]]=-1;
+      }
+        foreach(var x in dict){
+            if(x.Value>=1)return false;
         }
         return true;
-        
     }
 }
