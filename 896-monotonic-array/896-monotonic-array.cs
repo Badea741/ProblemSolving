88ae288b227639cf3edfbead79d2c479eeaff197
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-06-04 15:22:07 +0200

    Time: 547 ms (5.43%), Space: 52.9 MB (42.99%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cs
+++ 896-monotonic-array/896-monotonic-array.cs
@@ -1,20 +1,18 @@
 public class Solution
 {
-    public static bool CompareArray(int[] nums1,int[] nums2){
-        for(int i =0; i<nums1.Length;i++){
-            if(nums1[i]!=nums2[i])return false;
-        }
-        return true;
-    }
+   
     public  bool IsMonotonic(int[] nums)
     {
-        int[] notSorted = new int[nums.Length] ;
-        Array.Copy(nums, notSorted, nums.Length);
-        Array.Sort(nums);
-        if (CompareArray(notSorted,nums)) return true;
-        Array.Reverse(nums);
-        if (CompareArray(notSorted,nums)) return true;
-        return false;
+        bool increasing=true;
+        bool decreasing =true;
+        for(int i =0;i<nums.Length-1;i++){
+            if(nums[i]>nums[i+1]){
+                increasing=false;
+            }
+            if(nums[i]<nums[i+1])
+                decreasing=false;
+        }
+        return increasing||decreasing;
     }
    
 }
