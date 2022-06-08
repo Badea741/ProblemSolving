7b113fac4375c86f2da6ad02320a1b590ba31860
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-06-08 21:39:01 +0200

    Time: 22 ms (92.84%), Space: 25.3 MB (82.17%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cs
+++ 7-reverse-integer/7-reverse-integer.cs
@@ -5,47 +5,20 @@ public class Solution
     public int Reverse(int x)
     {
         int newInt = 0;
-        int i = 0;
-        StringBuilder builder = new();
-        if (x < 0)
-        {
-            while (x <= -1)
-            {
-                newInt += (x % 10) * (int)Math.Pow(10, i);
-                x = x / 10;
-                i++;
 
-            }
-
-            builder.Append(newInt);
-            char[] convertedString0 = builder.ToString().ToArray();
-            Array.Reverse(convertedString0);
-            try
-            {
-                return -int.Parse(new String(convertedString0).Substring(0, convertedString0.Length - 1));
-            }
-            catch (OverflowException)
-            {
-                return 0;
-            }
-        }
-        while (x >= 1)
+        while (x != 0)
         {
-            newInt += (x % 10) * (int)Math.Pow(10, i);
+            int pop = (x % 10);
             x = x / 10;
-            i++;
+            int compareResult=newInt;
+           
+         newInt = newInt * 10 + pop;
+            if((((newInt)-pop)/10)!=compareResult)return 0;
+            
         }
 
-        builder.Append(newInt);
-        char[] convertedString1 = builder.ToString().ToArray();
-        Array.Reverse(convertedString1);
-        try
-        {
-            return int.Parse(new String(convertedString1));
-        }
-        catch (OverflowException)
-        {
-            return 0;
-        }
+        return newInt;
+
     }
+
 }
