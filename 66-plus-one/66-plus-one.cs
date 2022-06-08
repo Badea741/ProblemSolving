f4e97482018f5c49289ab3900b5508cb28d6a164
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-06-08 02:04:03 +0200

    Time: 172 ms (55.75%), Space: 40.9 MB (73.97%) - LeetHub

--- 66-plus-one/66-plus-one.cs
+++ 66-plus-one/66-plus-one.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public int[] PlusOne(int[] digits) {
-       int i = digits.Length - 1;
+        int i = digits.Length - 1;
         int lastPosition = digits.Length - 1;
         int mostSignificantDigit = digits[lastPosition];
         List<int> alist = new();
@@ -9,28 +9,28 @@ public class Solution {
         {
             digits[lastPosition]++;
             alist = digits.ToList();
+            return alist.ToArray();
         }
-        else
+        if (lastPosition == 0) { alist.Add(0); alist.Add(1); }
+
+        while (digits[i] == 9 && i > 0)
         {
-            if (lastPosition == 0) { alist.Add(0); alist.Add(1); }
-            while (digits[i] == 9 && i > 0)
-            {
-                digits[i] = 0;
-                i--;
-            }
-            if (digits[i] < 9 && mostSignificantDigit == 9)
-            {
-                digits[i]++;
-                alist = digits.ToList();
-            }
-            else
-            {
-                digits[i] = 0;
-                alist = digits.ToList();
-                alist.Insert(0, 1);
-            }
+            digits[i] = 0;
+            i--;
         }
+        if (digits[i] < 9 && mostSignificantDigit == 9)
+        {
+            digits[i]++;
+            alist = digits.ToList();
+            return alist.ToArray();
+        }
+
+        digits[i] = 0;
+        alist = digits.ToList();
+        alist.Insert(0, 1);
+
         return alist.ToArray();
+
 
 
     }
