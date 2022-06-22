ac2d0b91c3b7834a1c46f32ea7e2e96582afb635
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-06-22 17:29:26 +0200

    Time: 84 ms (99.38%), Space: 40 MB (14.21%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
@@ -1,9 +1,6 @@
 public class Solution {
     public int FindKthLargest(int[] nums, int k) {
-        List<int> numsList=nums.ToList();
-        for(int i =0;i<k-1;i++){
-            numsList.Remove(numsList.Max());
-        }
-        return numsList.ToArray().Max();
+        Array.Sort(nums);
+        return nums[nums.Length-k];
     }
 }
