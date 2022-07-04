b447f68115b4c5f03c75e804ec84ac560c2d9e03
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-07-04 21:18:34 +0200

    Time: 120 ms (11.83%), Space: 35.6 MB (30.36%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cs
+++ 28-implement-strstr/28-implement-strstr.cs
@@ -1,11 +1,14 @@
 public class Solution {
+    // public int StrStr(string haystack, string needle) {
+    //    for(int i=0;;i++)
+    //        for(int j = 0;;j++){
+    //            if(j>=needle.Length)return i;
+    //            if(i+j>=haystack.Length)return -1;
+    //            if(haystack[i+j]!=needle[j])break;
+    //        }
+    //     return -1;
+    // }
     public int StrStr(string haystack, string needle) {
-       for(int i=0;;i++)
-           for(int j = 0;;j++){
-               if(j>=needle.Length)return i;
-               if(i+j>=haystack.Length)return -1;
-               if(haystack[i+j]!=needle[j])break;
-           }
-        return -1;
+       return haystack.IndexOf(needle);
     }
 }
