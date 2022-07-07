33994e22adc50a6d6155ee1a30b9cd037c6759ba
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-07-07 22:56:16 +0200

    Time: 113 ms (17.62%), Space: 35.9 MB (40.62%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cs
+++ 58-length-of-last-word/58-length-of-last-word.cs
@@ -1,7 +1,8 @@
 public class Solution {
     public int LengthOfLastWord(string s) {
-        s=s.Trim();
-        string[] arr=s.Split();
-        return arr[arr.Length-1].Length;
+        // s=s.Trim();
+        // string[] arr=s.Split();
+        return s.Trim().Split()[^1].Length;
+        // return arr[arr.Length-1].Length;
     }
 }
