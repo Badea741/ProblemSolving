7b46427e76731e0f1b947989e33cecdacf84fd49
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-07-24 14:22:42 +0200

    Time: 455 ms (14.76%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,1 +1,1 @@
-select Max(salary) SecondHighestSalary from  Employee where salary not in (select Max(salary) from Employee) 
+select(select distinct salary   from employee order by salary desc limit 1 offset 1) SecondHighestSalary
