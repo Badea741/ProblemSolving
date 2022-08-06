700785626e30c7e0b79d822c8d05795bc768895c
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-08-06 20:00:23 +0200

    Time: 43 ms (39.45%), Space: 25.1 MB (79.11%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MySqrt(int x) {
+        int high =x; int low=0;
+        while(high>=low){
+            int middle=low +(high-low)/2;
+            long t = (long)middle*middle;
+            if(t==x)return middle;
+            if(x<t){
+                high=middle-1;
+            }
+            else low=middle+1;
+        }
+        return high;
+    }
+}
