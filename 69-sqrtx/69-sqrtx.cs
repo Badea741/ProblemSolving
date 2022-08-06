808cf70432238319246291b66d80d8427746d34b
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-08-06 20:04:45 +0200

    Time: 35 ms (62.46%), Space: 25.4 MB (46.39%) - LeetHub

--- 69-sqrtx/69-sqrtx.cs
+++ 69-sqrtx/69-sqrtx.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int MySqrt(int x) {
         int high =x; int low=0;
         while(high>=low){
-            int middle=low+(high-low)/2;
+            int middle=(high+low)/2;
             long t = (long)middle*middle;
             if(t==x)return middle;
             if(x<t){
