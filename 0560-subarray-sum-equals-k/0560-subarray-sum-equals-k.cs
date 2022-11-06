c72a51272c55e7e04c3870650caff53683e5ebcd
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-11-06 19:01:33 +0200

    Time: 3740 ms (5.15%), Space: 41.7 MB (99.33%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
@@ -1,18 +1,32 @@
+public static class PrefixSumExtension
+{
+    
+    public static int[] PrefixSum(this int[] array)
+    {
+        int length = array.Length;
+        for (int i = 1; i < length; i++)
+        {
+            array[i] += array[i - 1];
+        }
+        return array;
+    }
+    public static int RangeSum(this int[] prefix, int start, int end)
+    {
+        if (start == 0) return prefix[end];
+        return prefix[end] - prefix[start - 1];
+    }
+}
 public class Solution {
     public int SubarraySum(int[] nums, int k) {
         int result = 0;
-        int sum = 0;
         int length = nums.Length;
+        int[] prefix = nums.PrefixSum();
         for (int i = 0; i < length; i++)
-        {
-            sum = 0;
             for (int j = i; j < length; j++)
             {
-                sum += nums[j];
-                if (sum == k)
+                if (prefix.RangeSum(i, j) == k)
                     result++;
             }
-        }
         return result;
     }
 }
