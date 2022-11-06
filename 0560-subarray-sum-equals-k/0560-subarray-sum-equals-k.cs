892700db39a88d23383ff1b3452829b6f1f751c3
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-11-06 20:43:26 +0200

    Time: 252 ms (62.86%), Space: 43.2 MB (44.52%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
@@ -20,13 +20,23 @@ public class Solution {
     public int SubarraySum(int[] nums, int k) {
         int result = 0;
         int length = nums.Length;
-        int[] prefix = nums.PrefixSum();
+        Dictionary<int, int> SumCount = new();
+                SumCount[0] = 1;
+
+        int prefixSum = 0;
         for (int i = 0; i < length; i++)
-            for (int j = i; j < length; j++)
+        {
+            prefixSum += nums[i];
+            if (SumCount.ContainsKey(prefixSum - k))
             {
-                if (prefix.RangeSum(i, j) == k)
-                    result++;
+                result += SumCount[prefixSum - k];
             }
+            if (!SumCount.ContainsKey(prefixSum))
+                SumCount[prefixSum] = 1;
+            else
+                SumCount[prefixSum]++;
+        }
+
         return result;
     }
 }
